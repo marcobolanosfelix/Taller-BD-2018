cc3444d18cea934b7b2071a01ee43ec585298391
marcobolanosfelix <32964606+marcobolanosfelix@users.noreply.github.com>
2018-09-27 18:31:11 -0600

    Update 01.CreacionConquistadores.sql

--- 01.CreacionConquistadores.sql
+++ 01.CreacionConquistadores.sql
@@ -2,24 +2,24 @@ create database Conquistadores;
 use Conquistadores;
 
 create table Pais (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null
 );
 
 create table Estado (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
     pais_id int
 );
 
 create table Ciudad (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
     estado_id int
 );
 
 create table Club (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
     lema varchar(100) not null,
     logo varchar(100) not null,
@@ -32,31 +32,31 @@ create table Club (
 );
 
 create table Director (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
     edad int not null
 );
 
 create table Subdirector (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
     edad int not null
 );
 
 create table Secretario (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
     edad int not null
 );
 
 create table Tesorero (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
     edad int not null
 );
 
 create table Instructor (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
     edad int not null,
     fecha_nacmiento date not null,
@@ -65,7 +65,7 @@ create table Instructor (
 );
 
 create table Clase (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
     logotipo varchar(50) not null,
     color varchar(30) not null,
@@ -74,15 +74,15 @@ create table Clase (
 );
 
 create table Padre (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
-    telefono int(10) not null,
+    telefono int not null,
     ocupacion varchar(50) not null,
     niño_id int
 );
 
 create table Niño (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
     fecha_nacimiento date not null,
     edad int not null,
@@ -91,7 +91,7 @@ create table Niño (
     numero_emergencia int not null,
     enfermedades varchar(200) not null,
     alergias varchar(200) not null,
-    mas_cumplido boolean,
+    mas_cumplido bit,
     unidad_id int,
     clase_id int
 );
@@ -99,21 +99,21 @@ create table Niño (
 create table Reunion_Niño ( -- Asistencia de cada Niño
     reunion_id int,
     niño_id int,
-    fecha timestamp,
-    uniforme boolean,
-    puntualidad boolean,
-    cuota boolean,
-    tarea boolean,
-    pulcritud boolean
+    fecha date,
+    uniforme bit,
+    puntualidad bit,
+    cuota bit,
+    tarea bit,
+    pulcritud bit
 );
 
 create table Reunion (
-    id int not null auto_increment primary key,
-    fecha timestamp
+    id int not null identity primary key,
+    fecha date
 );
 
 create table Unidad (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
     logotipo varchar(50) not null,
     lema varchar(100) not null,
@@ -122,34 +122,34 @@ create table Unidad (
 );
 
 create table Consejero (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null
 );
 
 create table Consejero_Especialidad (
     consejero_id int,
     especialidad_id int,
-    fecha timestamp
+    fecha date
 );
 
 create table Especialidad (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null
 );
 
 create table Especialidad_Instructor (
     especialidad_id int,
     instructor_id int,
-    fecha timestamp
+    fecha date
 );
 
 create table Investidura (
-    id int not null auto_increment primary key,
-    fecha timestamp
+    id int not null identity primary key,
+    fecha date
 );
 
 create table Control (
-    fecha timestamp,
+    fecha date,
     investidura_id int,
     clase_id int,
     instructor_id int,
@@ -157,7 +157,7 @@ create table Control (
 );
 
 create table Actividad (
-    id int not null auto_increment primary key,
+    id int not null identity primary key,
     nombre varchar(50) not null,
     investidura_id int
 );
@@ -166,39 +166,3 @@ create table Niño_Actividad (
     niño_id int,
     actividad_id int
 );
-
-
-insert into Actividad(nombre) values("Encender fuego");
-insert into Actividad(nombre) values("Escalar arbol");
-
-insert into Ciudad(nombre) values("Culiacan");
-insert into Ciudad(nombre) values("Navolato");
-
-insert into Clase(nombre, logotipo, color) values("Clase A", "Logotipo Circulo", "Azul");
-insert into Clase(nombre, logotipo, color) values("Clase B", "Logotipo Cuadrado", "Rojo");
-
-insert into Club(nombre, lema, logo, direccion) 
-    values("Las Arenas", "Sobrevivencia en todo momento", "Logo Triangulo", "Calle Los Angeles #435");
-insert into Club(nombre, lema, logo, direccion) 
-    values("Los del Bosque", "Cada aprendizaje es una vida salvada", "Logo Rectangulo", "Calle Hidalgo #67");
-    
-insert into Consejero(nombre) values("Alberto Aguilar");
-insert into Consejero(nombre) values("Benito Bueno");    
-
-insert into Director(nombre, edad) values("Carlos Carrillo", 40);
-insert into Director(nombre, edad) values("Daniel Delgado", 43);
-
-insert into Especialidad(nombre) values("Escalador Experto");
-insert into Especialidad(nombre) values("Creador del Fuego Experto");
-
-insert into Estado(nombre) values("Sinaloa");
-insert into Estado(nombre) values("Durango");
-
-insert into Instructor(nombre, edad, fecha_nacmiento) values("Esteban Estolano", 30, '10-10-1988');
-insert into Instructor(nombre, edad, fecha_nacmiento) values("Fabian Felix", 32, '08-10-1986');
-
-insert into Niño(nombre, fecha_nacimiento, edad, estura, peso, numero_emergencia, enfermedades, alergias, mas_cumplido)
-    values("Gabriel Garcia", '09-05-1990', 38, 180, 75, 6721179899, "Ninguna", "Lacteos", false);
-insert into Niño(nombre, fecha_nacimiento, edad, estura, peso, numero_emergencia, enfermedades, alergias, mas_cumplido)
-    values("Ignacio Irizar", '09-05-1992', 36, 177, 80, 6722279899, "Ninguna", "Chocolate", true);
-    
